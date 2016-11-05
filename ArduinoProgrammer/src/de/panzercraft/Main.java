617a99b16ee3db798c4cdf06f4e5df2f41eefbb9
Paul Hagedorn <panzer1119@gmail.com>
2016-11-06 00:23:43 +0100

    Test

--- ArduinoProgrammer/src/de/panzercraft/Main.java
+++ ArduinoProgrammer/src/de/panzercraft/Main.java
@@ -12,7 +12,7 @@ package de.panzercraft;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("TestPenis");
+        System.out.println("Oh man");
     }
     
 }
